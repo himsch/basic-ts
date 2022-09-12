dca7ec99c4f01d274001f84706a4a511e34c630e
song <55rsearchco@gmail.com>
2022-09-12 14:08:06 +0900

    모든것을 뒤엎는 Composition?
    각각의 필요한 기능을 class로 정의해서 필요한 곳에서 가져다가 사용. ⇒ Composition

---
+++ 3-OOP/3-7-composition.ts
@@ -0,0 +1,134 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+    hasSugar?: boolean;
+  };
+
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+  }
+
+  class CoffeeMachine implements CoffeeMaker {
+    private static BEANS_GRAM_PER_SHOT: number = 7;
+    private coffeeBeans: number = 0;
+
+    public constructor(coffeeBeans: number, private milk: MilkFrothier, private sugar: SugarProvider) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error(`value for beans should be greater than 0`);
+      }
+      this.coffeeBeans += beans;
+    }
+
+    clean() {
+      console.log(`cleaning the machine`);
+    }
+
+    private grindBeans(shots: number) {
+      console.log(`grinding beans for ${shots}`);
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAM_PER_SHOT) {
+        throw new Error(`Not enough coffee beans!!`);
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAM_PER_SHOT;
+    }
+
+    private preheat(): void {
+      console.log(`heating up...`);
+    }
+
+    private extract(shots: number): CoffeeCup {
+      console.log(`Pulling ${shots} shots....`);
+      return {
+        shots,
+        hasMilk: false,
+      }
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      const coffee = this.extract(shots);
+      const sugarAdded = this.sugar.addSugar(coffee);
+      return this.milk.makeMilk(sugarAdded);
+    }
+  }
+
+  interface MilkFrothier {
+    makeMilk(cup: CoffeeCup): CoffeeCup;
+  }
+
+  interface SugarProvider {
+    addSugar(cup: CoffeeCup): CoffeeCup;
+  }
+
+  class CheapMilkSteamer implements MilkFrothier {
+    private steamMilk(): void {
+      console.log(`Steaming some milk....`);
+    }
+    makeMilk(cup: CoffeeCup): CoffeeCup {
+      this.steamMilk();
+      return {
+        ...cup,
+        hasMilk: true,
+      }
+    }
+  }
+  class FancyMilkSteamer implements MilkFrothier {
+    private steamMilk(): void {
+      console.log('Steaming some milk....');
+    }
+    makeMilk(cup: CoffeeCup): CoffeeCup {
+      this.steamMilk();
+      return {
+        ...cup,
+        hasMilk: true,
+      }
+    }
+  }
+  class NoMilk implements MilkFrothier {
+    makeMilk(cup: CoffeeCup): CoffeeCup {
+      return cup
+    }
+  }
+
+  class CandySugarMixer implements SugarProvider {
+    private getSugar(): boolean {
+      console.log(`getting some sugar..`);
+      return true;
+    }
+    addSugar(cup: CoffeeCup): CoffeeCup {
+      const sugar = this.getSugar();
+      return {
+        ...cup,
+        hasSugar: sugar,
+      }
+    }
+  }
+  class SugarMixer implements SugarProvider {
+    private getSugar(): boolean {
+      console.log('Getting some sugar form jar!!');
+      return true;
+    }
+    addSugar(cup: CoffeeCup): CoffeeCup {
+      const sugar = this.getSugar();
+      return {
+        ...cup,
+        hasSugar: sugar,
+      }
+    }
+  }
+  class NoSugar implements SugarProvider {
+    addSugar(cup: CoffeeCup): CoffeeCup {
+      return cup;
+    }
+  }
+
+  const cheapMilkMaker = new CheapMilkSteamer();
+  const candySugar = new CandySugarMixer();
+
+  const sweetLatteMachine = new CoffeeMachine(12, cheapMilkMaker, candySugar);
+}
