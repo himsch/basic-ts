350c835a3f6befd635656cd7376207ac3a67d5ef
song <55rsearchco@gmail.com>
2022-09-12 13:32:17 +0900

    Polymorphism 다형성

---
+++ 3-OOP/3-6-polymorphism.ts
@@ -0,0 +1,101 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+    hasSugar?: boolean;
+  };
+
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+  }
+
+  class CoffeeMachine implements CoffeeMaker {
+    private static BEANS_GRAM_PER_SHOTS: number = 7;
+    private coffeeBeans: number = 0;
+
+    constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    fillCoffeeBean(beans: number) {
+      if (beans < 0) {
+        throw new Error(`value for beans should be greater than 0`);
+      }
+      this.coffeeBeans += beans;
+    }
+
+    private grindBeans(shots: number) {
+      console.log(`griding beans for ${shots}`);
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAM_PER_SHOTS) {
+        throw new Error(`Not enough coffee beans!`);
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAM_PER_SHOTS;
+    }
+
+    private preheat(): void {
+      console.log(`heating up...`);
+    }
+
+    private extract(shots: number): CoffeeCup {
+      console.log(`Pulling ${shots} shots....`);
+      return {
+        shots,
+        hasMilk: false,
+      }
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+    }
+  }
+
+  class CaffeLatteMachine extends CoffeeMachine {
+    constructor(beans: number, public readonly serialNumber: string) {
+      super(beans);
+    }
+
+    private steamMilk() {
+      console.log(`Steaming milk...`);
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+      this.steamMilk();
+      return {
+        ...coffee,
+        hasMilk: true,
+      }
+    }
+  }
+
+  class SweetCoffeeMaker extends CoffeeMachine {
+    constructor(beans: number) {
+      super(beans);
+    }
+
+    private sugar(): void {
+      console.log(`sugar in`);
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+      this.sugar();
+      return {
+        ...coffee,
+        hasSugar: true,
+      }
+    }
+  }
+
+  const machines: CoffeeMaker[] = [
+    new CoffeeMachine(16),
+    new CaffeLatteMachine(16, '1'),
+    new SweetCoffeeMaker(12),
+  ]
+  machines.forEach(machine => {
+    console.log(`----------------------`);
+    machine.makeCoffee(1);
+  })
+}
